71a0ed92cffef37e11d40bcfc520de9ea36c4d6b
Șeighiț Mihail <seighitm@mail.ru>
2020-05-04 22:37:59 +0300

    Initial Commit

---
+++ src/main/java/org/project/app/application/Main.java
@@ -0,0 +1,29 @@
+package org.project.app.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) {
+
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/Page.fxml"));
+            Scene scene = new Scene(root, 800, 600);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primaryStage.setResizable(false);
+            primaryStage.sizeToScene();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
