f61735bec9d67a33025eba50e208eed5e55c3757
Șeighiț Mihail <seighitm@mail.ru>
2020-05-19 12:30:33 +0300

    [PTA-41] Create a local database connection

---
+++ src/main/java/org/project/app/application/Connection/Configs.java
@@ -0,0 +1,9 @@
+package org.project.app.application.Connection;
+
+public class Configs {
+    protected static String dbhost = "localhost";
+    protected static String dbport = "3306";
+    protected static String dbuser = "root";
+    protected static String dbpass = "toor";
+    protected static String dbname = "genius";
+}
