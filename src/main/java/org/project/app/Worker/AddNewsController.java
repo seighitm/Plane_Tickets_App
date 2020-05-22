47f3457500c42da3fe18624313d1aa567ed01f9a
Șeighiț Mihail <seighitm@mail.ru>
2020-05-23 01:28:46 +0300

    [PTA-35] Creating the database connection

--- src/main/java/org/project/app/Worker/AddNewsController.java
+++ src/main/java/org/project/app/Worker/AddNewsController.java
@@ -2,15 +2,18 @@ package org.project.app.Worker;
 
 import java.net.URL;
 import javafx.fxml.FXML;
+import java.sql.Connection;
+import org.project.app.Connection.DBHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
 
 public class AddNewsController implements Initializable {
-    
+
     @FXML
     private AnchorPane field_page;
 
@@ -20,9 +23,19 @@ public class AddNewsController implements Initializable {
     @FXML
     private TextArea field_text;
 
+    @FXML
+    private DBHandler handler;
+
+    @FXML
+    private PreparedStatement pst;
+
+    @FXML
+    private Connection connection;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        handler = new DBHandler();
+        connection = handler.getConnection();
     }
 
     @FXML
