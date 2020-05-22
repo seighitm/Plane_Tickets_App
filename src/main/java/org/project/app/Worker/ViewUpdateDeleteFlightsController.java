8c941b35c2885f6e74a730b2d46f0d3301be3b53
Șeighiț Mihail <seighitm@mail.ru>
2020-05-22 15:32:06 +0300

    [PTA-68] Create a Controller class for FXML ViewFlight page

--- src/main/java/org/project/app/Worker/ViewUpdateDeleteFlightsController.java
+++ src/main/java/org/project/app/Worker/ViewUpdateDeleteFlightsController.java
@@ -1,17 +1,28 @@
 package org.project.app.Worker;
 
+import org.project.app.Connection.DBHandler;
 import org.project.app.Model.ModelViewFlight;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import org.project.app.LogIn_SignUp.LoginController;
 
 public class ViewUpdateDeleteFlightsController implements Initializable{
 
@@ -74,17 +85,25 @@ public class ViewUpdateDeleteFlightsController implements Initializable{
     private TableColumn<ModelViewFlight, String> hour_table;
     @FXML
     private TableColumn<ModelViewFlight, String> seats_table;
-    @FXML
-    private TableColumn<ModelViewFlight, String> action_table;
+
+    private DBHandler handler;
+    private PreparedStatement pst;
+    private Connection connection;
+
+
+    ObservableList<ModelViewFlight> oblist = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        handler = new DBHandler();
+        connection = handler.getConnection();
+        create_viewTable();
     }
 
     @FXML
-    void back(MouseEvent event) {
-
+    void back(MouseEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/Page.fxml"));
+        view_page.getChildren().setAll(pane);
     }
 
     @FXML
@@ -92,9 +111,34 @@ public class ViewUpdateDeleteFlightsController implements Initializable{
 
     }
 
-    @FXML
-    public void refresh_button(MouseEvent mouseEvent) {
+    void create_viewTable()
+    {
+        delete_label.setVisible(false);
+        notFound_label.setVisible(false);
+        update_label.setVisible(false);
+        notID_label.setVisible(false);
+        try {
+            pst = connection.prepareStatement("SELECT * from tab2");
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                oblist.add(new ModelViewFlight(rs.getInt("ID"), rs.getString("Location"), rs.getString("Destination"), rs.getString("Date"), rs.getInt("Price"), rs.getInt("Hour"),rs.getInt("Seats")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        id_table.setCellValueFactory(new PropertyValueFactory("ID"));
+        location_table.setCellValueFactory(new PropertyValueFactory("Location"));
+        destination_table.setCellValueFactory(new PropertyValueFactory("Destination"));
+        date_table.setCellValueFactory(new PropertyValueFactory("Date"));
+        price_table.setCellValueFactory(new PropertyValueFactory("Price"));
+        hour_table.setCellValueFactory(new PropertyValueFactory("Hour"));
+        seats_table.setCellValueFactory(new PropertyValueFactory("Seats"));
 
+        table.setItems(oblist);
+    }
+
+    public void refresh_button(MouseEvent mouseEvent) {
+        refresh_automatic();
     }
 
     public void search_byID(MouseEvent mouseEvent) {
@@ -106,5 +150,27 @@ public class ViewUpdateDeleteFlightsController implements Initializable{
     }
 
     public void update(MouseEvent mouseEvent) {
+
+    }
+
+    void refresh_automatic() {
+        table.getItems().clear();
+        try {
+            pst = connection.prepareStatement("SELECT * from tab2");
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                oblist.add(new ModelViewFlight(rs.getInt("ID"), rs.getString("Location"), rs.getString("Destination"), rs.getString("Date"), rs.getInt("Price"), rs.getInt("Hour"), rs.getInt("Seats")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        id_table.setCellValueFactory(new PropertyValueFactory("ID"));
+        location_table.setCellValueFactory(new PropertyValueFactory("Location"));
+        destination_table.setCellValueFactory(new PropertyValueFactory("Destination"));
+        date_table.setCellValueFactory(new PropertyValueFactory("Date"));
+        price_table.setCellValueFactory(new PropertyValueFactory("Price"));
+        hour_table.setCellValueFactory(new PropertyValueFactory("Hour"));
+        seats_table.setCellValueFactory(new PropertyValueFactory("Seats"));
+        table.setItems(oblist);
     }
 }
