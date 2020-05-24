f207b5e7d75c07bc16aac1455dde70124613706c
Șeighiț Mihail <seighitm@mail.ru>
2020-05-24 15:55:57 +0300

    [PTA-37] Creating the flight viewing method in controller class

--- src/main/java/org/project/app/Client/ViewFlightsController.java
+++ src/main/java/org/project/app/Client/ViewFlightsController.java
@@ -1,5 +1,6 @@
 package org.project.app.Client;
 
+import javafx.util.Callback;
 import org.project.app.Connection.DBHandler;
 import org.project.app.Model.ModelViewFlight;
 import javafx.collections.FXCollections;
@@ -9,10 +10,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.Button;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.scene.input.MouseEvent;
 
@@ -57,6 +63,80 @@ public class ViewFlightsController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         handler = new DBHandler();
         connection = handler.getConnection();
+        view_flight();
+    }
+
+    public void refresh_automation() {
+        table.getItems().clear();
+        try {
+            pst = connection.prepareStatement("SELECT * from tab2");
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()) {
+                if (rs.getInt("Seats") > 0) {
+                    oblist.add(new ModelViewFlight(rs.getInt("ID"), rs.getString("Location"), rs.getString("Destination"), rs.getString("Date"), rs.getInt("Price"), rs.getInt("Hour"), rs.getInt("Seats")));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        id_table.setCellValueFactory(new PropertyValueFactory("ID"));
+        location_table.setCellValueFactory(new PropertyValueFactory("Location"));
+        destination_table.setCellValueFactory(new PropertyValueFactory("Destination"));
+        date_table.setCellValueFactory(new PropertyValueFactory("Date"));
+        price_table.setCellValueFactory(new PropertyValueFactory("Price"));
+        hour_table.setCellValueFactory(new PropertyValueFactory("Hour"));
+        seats_table.setCellValueFactory(new PropertyValueFactory("Seats"));
+
+        table.setItems(oblist);
+    }
+
+    public void view_flight()
+    {
+        refresh_automation();
+
+        Callback<TableColumn<ModelViewFlight, String>, TableCell<ModelViewFlight, String>> cellFactory
+                = //
+                new Callback<TableColumn<ModelViewFlight, String>, TableCell<ModelViewFlight, String>>() {
+                    @Override
+                    public TableCell call(final TableColumn<ModelViewFlight, String> param) {
+                        final TableCell<ModelViewFlight, String> cell = new TableCell<ModelViewFlight, String>() {
+
+                            final Button btn = new Button("BUY");
+
+                            @Override
+                            public void updateItem(String item, boolean empty) {
+                                super.updateItem(item, empty);
+                                if (empty) {
+                                    setGraphic(null);
+                                    setText(null);
+                                } else {
+                                    btn.setOnAction(event -> {
+                                        ModelViewFlight curse = getTableView().getItems().get(getIndex());
+                                        String insert = "UPDATE tab2 SET Seats=? WHERE ID=?";
+                                        try {
+                                            pst = connection.prepareStatement(insert);
+                                            int aux = curse.getSeats()-1;
+                                            curse.setSeats(aux);
+                                            pst.setInt(1, curse.getSeats());
+                                            pst.setInt(2, curse.getID());
+                                            pst.executeUpdate();
+                                            refresh_automation();
+                                        } catch (SQLException e) {
+                                            e.printStackTrace();
+                                        }
+                                    });
+                                    setGraphic(btn);
+                                    setText(null);
+                                }
+                            }
+                        };
+                        return cell;
+                    }
+                };
+        button_table.setCellFactory(cellFactory);
+
+        table.setItems(oblist);
     }
 
     public void refresh_button(MouseEvent mouseEvent) {
