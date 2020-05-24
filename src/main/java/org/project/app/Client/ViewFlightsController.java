5d17998ee9dbdf710a132ec4ec4fb57c5d729f66
Șeighiț Mihail <seighitm@mail.ru>
2020-05-24 15:37:37 +0300

    [PTA-38] Creating the form to display all current flights.

---
+++ src/main/java/org/project/app/Client/ViewFlightsController.java
@@ -0,0 +1,14 @@
+package org.project.app.Client;
+
+import javafx.scene.input.MouseEvent;
+
+public class ViewFlightsController {
+    public void refresh_button(MouseEvent mouseEvent) {
+    }
+
+    public void back(MouseEvent mouseEvent) {
+    }
+
+    public void search_flight(MouseEvent mouseEvent) {
+    }
+}
