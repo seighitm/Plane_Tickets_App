242fa923c6e680167a9c285cf652bcbb54152120
Șeighiț Mihail <seighitm@mail.ru>
2020-05-25 12:16:53 +0300

    [PTA-77] Creating a home page GUI for non-logged in User

---
+++ src/main/java/org/project/app/NonLoggedUser/HomeSkipPage.java
@@ -0,0 +1,14 @@
+package org.project.app.NonLoggedUser;
+
+import javafx.scene.input.MouseEvent;
+
+public class HomeSkipPage {
+    public void view_flights(MouseEvent mouseEvent) {
+    }
+
+    public void exit(MouseEvent mouseEvent) {
+    }
+
+    public void gen_inf(MouseEvent mouseEvent) {
+    }
+}
